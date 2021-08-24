52492dcc54fe2d67bbb9b16ec9311e797a204681
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-08-24 16:30:13 -0300

    CircuitBreaker

--- currency-exchange-service/src/main/java/com/inacioalves/web/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/inacioalves/web/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
@@ -18,10 +19,10 @@ public class CircuitBreakerController {
 				LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	//@RateLimiter(name="default")
-	@Bulkhead(name="sample-api")
+//	@Bulkhead(name="sample-api")
 	//10s => 10000 calls to the sample api
 	public String sampleApi() {
 		logger.info("Sample api call received");
